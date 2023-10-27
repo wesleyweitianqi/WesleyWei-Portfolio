8b8d3707b140320c6a5643b3cc018b2a65a24409
wesleyweitianqi <wesleytianqiwei@gmail.com>
2023-10-26 22:15:02 -0400

    new Portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,134 +7,133 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
-
 //template_ywohe4y
 //service_eyhfkf9
 //bU5RUIvP3ARtdSjYJ
 const Contact = () => {
-    const formRef = useRef();
-    const [form, setForm] = useState({
-        name: "",
-        email: "",
-        message: "",
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
     });
+  };
 
-    const [loading, setLoading] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    const handleChange = (e) => {
-        const { target } = e;
-        const { name, value } = target;
+    emailjs
+      .send(
+        "service_yu4c8dm",
+        "template_i78bhvn",
+        {
+          from_name: form.name,
+          to_name: "Wesley",
+          from_email: form.email,
+          to_email: "wesleytianqiwei@gmail.com",
+          message: form.message,
+        },
+        "-nJVp2ed5qFzq9vzs"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
 
-        setForm({
-            ...form,
-            [name]: value,
-        });
-    };
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setLoading(true);
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
-        emailjs
-            .send(
-                "service_eyhfkf9",
-                "template_ywohe4y",
-                {
-                    from_name: form.name,
-                    to_name: "Xinhan Yang",
-                    from_email: form.email,
-                    to_email: "ambpoeialxi@gmail.com",
-                    message: form.message,
-                },
-               "bU5RUIvP3ARtdSjYJ"
-            )
-            .then(
-                () => {
-                    setLoading(false);
-                    alert("Thank you. I will get back to you as soon as possible.");
+  return (
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-                    setForm({
-                        name: "",
-                        email: "",
-                        message: "",
-                    });
-                },
-                (error) => {
-                    setLoading(false);
-                    console.error(error);
-
-                    alert("Ahh, something went wrong. Please try again.");
-                }
-            );
-    };
-
-    return (
-        <div
-            className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
         >
-            <motion.div
-                variants={slideIn("left", "tween", 0.2, 1)}
-                className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
-            >
-                <p className={styles.sectionSubText}>Get in touch</p>
-                <h3 className={styles.sectionHeadText}>Contact.</h3>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your good name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your web address?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
 
-                <form
-                    ref={formRef}
-                    onSubmit={handleSubmit}
-                    className='mt-12 flex flex-col gap-8'
-                >
-                    <label className='flex flex-col'>
-                        <span className='text-white font-medium mb-4'>Your Name</span>
-                        <input
-                            type='text'
-                            name='name'
-                            value={form.name}
-                            onChange={handleChange}
-                            placeholder="What's your good name?"
-                            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-                        />
-                    </label>
-                    <label className='flex flex-col'>
-                        <span className='text-white font-medium mb-4'>Your email</span>
-                        <input
-                            type='email'
-                            name='email'
-                            value={form.email}
-                            onChange={handleChange}
-                            placeholder="What's your web address?"
-                            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-                        />
-                    </label>
-                    <label className='flex flex-col'>
-                        <span className='text-white font-medium mb-4'>Your Message</span>
-                        <textarea
-                            rows={7}
-                            name='message'
-                            value={form.message}
-                            onChange={handleChange}
-                            placeholder='What you want to say?'
-                            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-                        />
-                    </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
 
-                    <button
-                        type='submit'
-                        className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-                    >
-                        {loading ? "Sending..." : "Send"}
-                    </button>
-                </form>
-            </motion.div>
-
-            <motion.div
-                variants={slideIn("right", "tween", 0.2, 1)}
-                className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-            >
-                <EarthCanvas />
-            </motion.div>
-        </div>
-    );
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
 };
 
 export default SectionWrapper(Contact, "contact");
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,303 +1,288 @@
 import {
-    mobile,
-    backend,
-    creator,
-    web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    git,
-    figma,
-    docker,
-    carrent,
-    jobit,
-    tripguide,
-    threejs, starbucks, meta, tesla,
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  threejs,
+  starbucks,
+  meta,
+  tesla,
 } from "../assets";
-import sample from '../assets/sample.png'
+import bookinterview from "../assets/bookinterview.png";
+import courseStudy from "../assets/courseStudy.png";
+import payment from "../assets/payment.png";
+import sample from "../assets/sample.png";
 export const navLinks = [
-    {
-        id: "about",
-        title: "About",
-    },
-    {
-        id: "work",
-        title: "Work",
-    },
-    {
-        id: "contact",
-        title: "Contact",
-    },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
 ];
 
 const services = [
-    {
-        title: "Web Developer",
-        icon: web,
-    },
-    {
-        title: "Full Stack Developer",
-        icon: mobile,
-    },
-    {
-        title: "TypeScript Developer",
-        icon: backend,
-    },
-    {
-        title: "Front End Developer",
-        icon: creator,
-    },
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: mobile,
+  },
+  {
+    title: "Java Developer",
+    icon: backend,
+  },
+  {
+    title: "Front End Developer",
+    icon: creator,
+  },
 ];
 
 const technologies = [
-    {
-        name: "HTML 5",
-        icon: html,
-    },
-    {
-        name: "CSS 3",
-        icon: css,
-    },
-    {
-        name: "JavaScript",
-        icon: javascript,
-    },
-    {
-        name: "TypeScript",
-        icon: typescript,
-    },
-    {
-        name: "React JS",
-        icon: reactjs,
-    },
-    {
-        name: "Redux Toolkit",
-        icon: redux,
-    },
-    {
-        name: "Tailwind CSS",
-        icon: tailwind,
-    },
-    {
-        name: "Node JS",
-        icon: nodejs,
-    },
-    {
-        name: "Three JS",
-        icon: threejs,
-    },
-    {
-        name: "git",
-        icon: git,
-    },
-    {
-        name: "figma",
-        icon: figma,
-    },
-    {
-        name: "docker",
-        icon: docker,
-    },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
 ];
 
 const experiences = [
-    {
-        title: "Web Developer",
-        company_name: "MentorX",
-        icon: tesla,
-        iconBg: "#383E56",
-        date: "Jun 2021 - Sep 2021",
-        points: [
-            "Created an online learning e-commerce platform SPA by using React, React-Redux, Next.js with various components\n" +
-            "and using React Hook and universal store using Redux, algorithm simulation and realization based on Next.js and \n" +
-            "React.js with multiple components.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-            "Specific projects such as the GitHub merging and Front-end pages implement from figma, improvement evaluation \n" +
-            "and code reviewing, with launching and deployment of SDLC and AWS etc"
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Mark2Win",
-        icon: meta,
-        iconBg: "#E6DEDD",
-        date: "Sep 2021 - Sep 2022",
-        points: [
-            "Created an e-commerce platform SPA by using React, React-Redux, Redux-Form, with (Express & NodeJS) as REST \n" +
-            "API backend to talk to database (MySQL) followed responsive and mobile first rules to design layout and components \n" +
-            "data driven dynamic applications for rendering full accessible user interfaces",
-            "Created various components and using React Hook and universal store using Redux.\n",
-            "Implemented the RBAC (Role based access control), supported role and permission assignment and access.\n",
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-
-    {
-        title: "JavaScript Developer",
-        company_name: "LearnFormula.Inc",
-        icon: starbucks,
-        iconBg: "#383E56",
-        date: "Dec 2022 - March 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Migrated the whole system from VueJs to ReactJs Using NextAuth and Next Documentation Vercel",
-            "Created the e-commerce Online courses platform SPA by using Next13, Nuxt2, with (ReactJs, VueJs framework) \n" +
-            "and Laravel and PHP, as REST API backend to talk to database (MySQL) following responsive and mobile first\n" +
-            "rules to design layout and components with Bootstrap5 from Figma",
-            "Developed the Login and Register Pages, etc. and All Landing Pages as Custom-packages with data driven dynamic\n" +
-            "applications for rendering full accessible user interfaces"
-        ],
-    },
+  {
+    title: "Web Developer",
+    company_name: "Rex power magnetics",
+    icon: tesla,
+    iconBg: "#383E56",
+    date: "Oct 2022- Current",
+    points: [
+      "Spearheaded the development of an Inventory Management System, leveraging Spring Boot and React.js, adeptly serving a diverse user base of over 300 internal users. Ensured the robust and secure storage of critical data by orchestrating MySQL and MongoDB database integration.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Specific projects such as the GitHub merging and Front-end pages implement from figma, improvement evaluation \n" +
+        "and code reviewing, with launching and deployment of SDLC and AWS etc",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Lighthouse labs",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Jun 2022 - Sep 2022",
+    points: [
+      "Created an e-commerce platform SPA by using React, React-Redux, Redux-Form, with (Express & NodeJS) as REST \n" +
+        "API backend to talk to database (MySQL) followed responsive and mobile first rules to design layout and components \n" +
+        "data driven dynamic applications for rendering full accessible user interfaces",
+      "Created various components and using React Hook and universal store using Redux.\n",
+      "Implemented the RBAC (Role based access control), supported role and permission assignment and access.\n",
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
 ];
 
 const testimonials = [
-    {
-        testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
-        designation: "CFO",
-        company: "Acme Co",
-        image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-        testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-        testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
-        designation: "CTO",
-        company: "456 Enterprises",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
 ];
 
 const projects = [
-    {
-        name: "City View",
-        description:
-            "Web-based platform that allows users to search, change, and manage city views from various providers, providing a convenient and background of images.",
-        tags: [
-            {
-                name: "react",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "node.js",
-                color: "green-text-gradient",
-            },
-            {
-                name: "scss",
-                color: "pink-text-gradient",
-            },
-        ],
-        image: carrent,
-        source_code_link: "https://github.com/abpoooo/CityView_Different_Page",
-    },
-    {
-        name: "LuLu Lemon clone ",
-        description:
-            "A LuLuLemon Clone using React, Redux and Node.js for the backend,Developed data driven dynamic applications for rendering full accessible user interfaces\n" +
-            "Followed responsive and mobile first rules to design layout and components\n" +
-            "Implemented the global filters in both Front-end UI and backend logics\n .",
-        tags: [
-            {
-                name: "react",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "restapi",
-                color: "green-text-gradient",
-            },
-            {
-                name: "scss",
-                color: "pink-text-gradient",
-            },
-            {
-                name: "redux",
-                color: "red-text-gradient",
-            },
-        ],
-        image: jobit,
-        source_code_link: "https://github.com/abpoooo/Lululemon_Final/",
-    },
-    {
-        name: "Custom-Package Landing Page for LearnFormula",
-        description:
-            "Created the e-commerce Online courses platform SPA by using Next13, Nuxt2, with (ReactJs, VueJs framework) \n" +
-            "and Laravel and PHP, as REST API backend to talk to database (MySQL) following responsive and mobile first\n" +
-            "rules to design layout and components with Bootstrap5 from Figma as a custom-package landing page for CPD Formula.",
-        tags: [
-            {
-                name: "next.js",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "scss",
-                color: "green-text-gradient",
-            },
-            {
-                name: "css",
-                color: "pink-text-gradient",
-            },
-            {
-                name: "react",
-                color: "red-text-gradient",
-            },
-            {
-                name: "Vue.js",
-                color: "orange-text-gradient",
-            },
-        ],
-        image: tripguide,
-        source_code_link: "https://cpdformula.com/custom-package?hours=20/",
-    },
-    {
-        name: "DashBoard Project",
-        description:
-            "Created the DashBoard platform SPA by using React, Syncfusion and tailwind to display a dashboard for graphs \n" +
-            "and displaying customers employees budgets and income charts in different ways with responsive design.",
-        tags: [
-            {
-                name: "react.js",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "scss",
-                color: "green-text-gradient",
-            },
-            {
-                name: "css",
-                color: "pink-text-gradient",
-            },
-            {
-                name: "tailwind",
-                color: "red-text-gradient",
-            },
-            {
-                name: "syncfusion",
-                color: "orange-text-gradient",
-            },
-        ],
-        image: sample,
-        source_code_link: "https://github.com/abpoooo/dashborad_project/",
-    },
+  {
+    name: "Scheduler",
+    description:
+      "Interview Scheduler is a SPA (Single Page Application) for tracking students interviews built by utilizing React built-in and custom hooks to allow users to add, edit and delete appointments in real time. Data is persisted by the API server using a PostgreSQL database. The client application communicates with an API server over HTTP, using the JSON format. In terms of testing, the project follows best practices of TDD (Test Driven Development), where individual Components are tested in isolation as well as End-to-End testing is performed.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "node.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Jest",
+        color: "red-text-gradient",
+      },
+      {
+        name: "Cypress",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: bookinterview,
+    source_code_link: "https://github.com/wesleyweitianqi/scheduler",
+  },
+  {
+    name: "courseProject ",
+    description:
+      "The Course Project is a Single Page Application (SPA) designed for course recommendation on Udemy for beginners. The project utilizes Vue3 and Element-plus UI, styled using Less to provide a user-friendly experience. The project also provides functionalities such as login, edit, delete, search and category reordering. The data is persisted by running Docker on an EC2 instance using a MYSQL image. The client-side communicates with the server over HTTP with JWT authentication, while the frontend uses Element UI's built-in form validation, and the backend uses JOI for data validation.",
+    tags: [
+      {
+        name: "vue",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "AWS EC2 S3",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mysql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "JWT",
+        color: "red-text-gradient",
+      },
+    ],
+    image: courseStudy,
+    source_code_link: "https://github.com/wesleyweitianqi/courseProject",
+  },
+  // {
+  //   name: "",
+  //   description:
+  //     "",
+  //   tags: [
+  //     {
+  //       name: "next.js",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "scss",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "react",
+  //       color: "red-text-gradient",
+  //     },
+  //     {
+  //       name: "Vue.js",
+  //       color: "orange-text-gradient",
+  //     },
+  //   ],
+  //   image: tripguide,
+  //   source_code_link: "",
+  // },
+  {
+    name: "Jungle",
+    description:
+      "Create a dynamic e-commerce platform using Ruby on Rails 6.1 with robust features, including secure payment processing through the Stripe API. The application leverages PostgreSQL for efficient database management and adopts server-side rendering (SSR) for improved user experience. Shop for your favorite products seamlessly with our user-friendly interface.",
+    tags: [
+      {
+        name: "Ruby on rails",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Stripe",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Bootstrap 5",
+        color: "red-text-gradient",
+      },
+      {
+        name: "PostgreSQL",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: payment,
+    source_code_link: "https://github.com/wesleyweitianqi/Jungle",
+  },
 ];
 
 export { services, technologies, experiences, testimonials, projects };
